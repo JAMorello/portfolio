bce764e4a8e7e836b15f0d032c860befcb11c0c9
JAMorello <juan.ag.morello@gmail.com>
2021-11-12 22:53:15 -0300

    Add title

--- src/pages/Acerca.js
+++ src/pages/Acerca.js
@@ -4,6 +4,9 @@ import ProfileCard from "../components/ProfileCard";
 function Acerca() {
   return (
     <div className="flex flex-col justify-center items-center my-10 ">
+      <h2 className="text-3xl sm:text-5xl font-bold text-white uppercase underline text-right mb-5">
+        Acerca de mí
+      </h2>
       <Conocimientos />
       <ProfileCard />
     </div>
