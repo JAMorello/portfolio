4f20806d723143e01f8afe104fb87e14c5012228
JAMorello <juan.ag.morello@gmail.com>
2021-11-12 22:01:48 -0300

    Update buttons style and adjust mobile behaviour of 'Acerca' page

--- src/pages/Acerca.js
+++ src/pages/Acerca.js
@@ -23,13 +23,21 @@ function Acerca() {
       <div className="w-full sm:w-2/3	p-6 grid grid-cols-1 md:grid-cols-3 bg-blue-100 border-2 rounded shadow-lg">
         <ProfileImg />
         <div className="col-span-1 md:col-span-2">
-          <p className="text-xl font-bold text-center text-blue-600 pb-2">
+          <p className="text-2xl mt-6 md:mt-0 font-bold text-center text-blue-600 pb-0 sm:pb-2">
             Mi recorrido como...
           </p>
-          <div className="flex flex-col pl-6 mt-10 md:mt-0">
+          <div className="flex flex-col pl-0 px-0 sm:px-2 md:pl-6 mt-5 md:mt-0">
             <div className="grid grid-cols-2 gap-x-4">
-              <BotonTextos text="Programador" handleClick={handleProgClick} />
-              <BotonTextos text="Filósofo" handleClick={handleFilClick} />
+              <BotonTextos
+                text="Programador"
+                active={showProg}
+                handleClick={handleProgClick}
+              />
+              <BotonTextos
+                text="Filósofo"
+                active={showFil}
+                handleClick={handleFilClick}
+              />
             </div>
             {showProg && <TextoProgramador />}
             {showFil && <TextoFilosofo />}
