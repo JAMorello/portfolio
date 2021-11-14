1316627e4dc6db935ff2c7384f7174e5c6a4ab44
JAMorello <juan.ag.morello@gmail.com>
2021-11-14 20:37:12 -0300

    Adjust responsiveness of modal-details component and its carousel of images

--- src/components/ModalProyecto.js
+++ src/components/ModalProyecto.js
@@ -1,37 +1,69 @@
+import { useState, useEffect } from "react";
 import { FaWindowClose } from "react-icons/fa";
 import TechBadge from "./TechBadge";
 import ProyectSlider from "./ProyectSlider";
 
 const ModalProyecto = ({ curProyecto, showModal, setShowModal }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = (e) => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => window.addEventListener("resize", handleResize));
+
+  const [sliderWidth, setSliderWidth] = useState("300px");
+
+  useEffect(() => {
+    switch (true) {
+      case windowWidth >= 1440:
+        setSliderWidth("600px");
+        break;
+      case windowWidth <= 425:
+        setSliderWidth("300px");
+        break;
+      default:
+        const px = 600 - (1440 - windowWidth) * 0.29;
+        setSliderWidth(`${px}px`);
+    }
+  }, [windowWidth]);
+
   return (
     <>
       {showModal ? (
         <>
           <div className="fixed flex justify-center items-center inset-0 z-50">
-            <div className="relative w-auto max-w-3xl my-6 mx-auto ">
+            <div className="relative w-full max-w-3xl my-6 mx-auto ">
               {/*content*/}
               <div className="relative flex flex-col w-full bg-blue-100 rounded-lg shadow-lg">
                 {/*header*/}
                 <div className="flex justify-between p-5 border-b border-solid border-blue-300 rounded-t">
-                  <h3 className="text-3xl font-semibold">{curProyecto.name}</h3>
+                  <h3 className="text-xl sm:text-3xl font-semibold">
+                    {curProyecto.name}
+                  </h3>
                   <button onClick={() => setShowModal(false)}>
                     <FaWindowClose className="w-8 h-8 text-red-600" />
                   </button>
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <ProyectSlider images={curProyecto.extraImgs} />
-                  <div class="flex my-4 gap-2 items-baseline">
+                  <div className="flex justify-center items-center">
+                    <ProyectSlider
+                      className="h-full"
+                      images={curProyecto.extraImgs}
+                      sliderWidth={sliderWidth}
+                    />
+                  </div>
+
+                  <div class="flex my-4 gap-2 flex-wrap items-baseline">
                     {curProyecto.technologies.map((e) => (
                       <TechBadge tech={e} />
                     ))}
                   </div>
-                  <p className="my-4 text-justify">
+                  <p className="my-4 text-xs sm:text-sm text-justify">
                     {curProyecto.extendedText}
                   </p>
                 </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 rounded-b"></div>
               </div>
             </div>
           </div>
--- src/components/ProyectSlider.js
+++ src/components/ProyectSlider.js
@@ -1,13 +1,39 @@
-import AwesomeSlider from "react-awesome-slider";
-import "react-awesome-slider/dist/styles.css";
+// import AwesomeSlider from "react-awesome-slider";
+// import withCaption from "react-awesome-slider/dist/captioned";
+// import "react-awesome-slider/dist/styles.css";
+// import CaptionedStyles from "react-awesome-slider/dist/captioned.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
-const ProyectSlider = ({ images }) => {
+const ProyectSlider = ({ images, sliderWidth }) => {
+  // const CaptionedSlider = withCaption(AwesomeSlider);
+
   return (
-    <AwesomeSlider>
+    <Carousel showThumbs={false} width={sliderWidth}>
       {images.map((e) => (
-        <div data-src={e} />
+        <div key={e.caption}>
+          <img className="object-cover" src={e.img} alt={e.caption} />
+          <p className="legend">{e.caption}</p>
+        </div>
       ))}
-    </AwesomeSlider>
+    </Carousel>
+    // <AwesomeSlider cssModule={AwsSliderStyles}>
+    //   {images.((e) => (
+    //     <div key={e.caption} data-src={e.img}>
+    //       <p>{e.caption}</p>
+    //     </div>
+    //   ))}
+    // </AwesomeSlider>
+    // <CaptionedSlider>
+    //   startupScreen={StartupScreen}
+    //   cssModule={CaptionedStyles}
+    //   screens=
+    //   {images.map((e) => ({
+    //     backgroundColor: "#4a9c8c",
+    //     media: e.img,
+    //     caption: e.caption,
+    //   }))}
+    // </CaptionedSlider>
   );
 };
 
